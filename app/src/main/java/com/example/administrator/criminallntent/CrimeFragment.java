81c7b9257800186c30cc1d69ca862c0c8182bd00
Pear_pear <bydcyh147@qq.com>
2017-01-24 14:27:48 +0800

    updata1212

---
+++ app/src/main/java/com/example/administrator/criminallntent/CrimeFragment.java
@@ -0,0 +1,14 @@
+package com.example.administrator.criminallntent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/1/24.
+ */
+/*
+gengxin
+*/
+
+public class CrimeFragment extends Fragment {
+
+}
